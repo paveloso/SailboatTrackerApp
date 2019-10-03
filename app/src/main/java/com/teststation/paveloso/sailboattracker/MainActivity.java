8b647a89a82ef2d45d21069848b3cfd1dcf35645
paveloso <33777232+paveloso@users.noreply.github.com>
2019-10-03 17:08:34 +0300

    if can't connect the show map button is disabled (#5)

--- app/src/main/java/com/teststation/paveloso/sailboattracker/MainActivity.java
+++ app/src/main/java/com/teststation/paveloso/sailboattracker/MainActivity.java
@@ -3,7 +3,10 @@ package com.teststation.paveloso.sailboattracker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.teststation.paveloso.sailboattracker.Utils.ConnectionCheckerAsync;
@@ -15,10 +18,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView connResultText = (TextView) findViewById(R.id.conn_res);
+        final TextView connResultText = (TextView) findViewById(R.id.conn_res);
+        final Button showMapButton = (Button) findViewById(R.id.showmap_but);
+        showMapButton.setEnabled(false);
+        showMapButton.setBackgroundColor(getResources().getColor(R.color.disabled_button));
 
         ConnectionCheckerAsync ccAsync = new ConnectionCheckerAsync(connResultText);
         ccAsync.execute();
+
+        connResultText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (connResultText.getText().toString().equals("Available")) {
+                    showMapButton.setEnabled(true);
+                    showMapButton.setBackgroundColor(getResources().getColor(R.color.app_buttons_red));
+                } else {
+                    showMapButton.setEnabled(false);
+                    showMapButton.setBackgroundColor(getResources().getColor(R.color.disabled_button));
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
     public void openRaceMap(View view) {
